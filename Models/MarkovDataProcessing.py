52f190fef72c0207faf9efc248ef1ae60145609b
viktorrei <viktor_reineck@yahoo.se>
2021-01-31 17:51:53 +0100

    minor simplifications

--- Models/MarkovDataProcessing.py
+++ Models/MarkovDataProcessing.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import numpy as np
 
-df = pd.read_csv('Analytics.csv', header=5)
+df = pd.read_csv('Analytics_raw_data_sample.csv', header=5)
 df = df.rename(columns={'Väg till konvertering per källa': 'path',
                         'Konverteringar': 'total_conversions',
                         'Konverteringsvärde': 'total_conversion_value'})
-df.insert(len(df.columns),'total_null',np.zeros(len(df), dtype=np.int))
+
+df['total_null'] = 0
 
 df['total_conversions'] = df['total_conversions'].str.replace('\s+','').astype(int)
 df['total_conversion_value'] = df['total_conversion_value'].str.rstrip('kr').\
