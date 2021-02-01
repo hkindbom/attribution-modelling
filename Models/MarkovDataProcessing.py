268b00f1a1c2ba8ecaec54e6e526809ecacb7e9c
viktorrei <viktor_reineck@yahoo.se>
2021-02-01 12:14:23 +0100

    markov model working

--- Models/MarkovDataProcessing.py
+++ Models/MarkovDataProcessing.py
@@ -10,11 +10,11 @@ def process_GA_data(file_path, save_to_path):
 
     df['total_conversions'] = df['total_conversions'].str.replace('\s+','').astype(int)
     df['total_conversion_value'] = df['total_conversion_value'].str.rstrip('kr').\
-        str.replace(',','.').str.replace('\s+','').astype(float).astype(int)
+        str.replace(',','.').str.replace('\s+','').astype(float)
 
     df.to_csv(save_to_path, index = False, sep=';')
 
 if __name__ == '__main__':
-    file_path = '../Data/Analytics_raw_data_sample.csv'
-    save_to_path = '../Data/GA_data_processed.csv'
+    file_path = '../Data/channel_journey_data.csv'
+    save_to_path = '../Data/channel_journey_data_processed.csv'
     process_GA_data(file_path, save_to_path)
--- Models/MarkovModel.py
+++ Models/MarkovModel.py
@@ -1,25 +1,34 @@
-from ChannelAttribution import auto_markov_model
+from ChannelAttribution import markov_model
+from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
 class MarkovModel:
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, order, show_top_results=False):
         self.dataframe = dataframe
         self.attributions = None
         self.transition_matrix = None
         self.removal_effects = None
+        self.order = order
+        self.show_top_results = show_top_results
 
     def train_all(self):
-        auto_model_raw = auto_markov_model(self.dataframe,
+
+        model_raw = markov_model(self.dataframe,
                                   var_path='path',
                                   var_conv='total_conversions',
                                   var_value='total_conversion_value',
                                   var_null='total_null',
-                                  out_more=True)
-        self.attributions = auto_model_raw['result']
-        self.transition_matrix = auto_model_raw['transition_matrix']
-        self.removal_effects = auto_model_raw['removal_effects']
+                                  out_more=True,
+                                  order=self.order)
+        if self.show_top_results:
+            nr_results = 15
+        else:
+            nr_results = -1
 
+        self.attributions = model_raw['result'].sort_values(by=['total_conversion_value'], ascending=False)[:nr_results]
+        self.transition_matrix = model_raw['transition_matrix'][:nr_results]
+        self.removal_effects = model_raw['removal_effects'].sort_values(by=['removal_effects_conversion_value'], ascending=False)[:nr_results]
 
     def show_results(self):
         self.plot_attributions()
@@ -28,7 +37,9 @@ class MarkovModel:
 
     def plot_attributions(self):
         ax = self.attributions.plot.bar('channel_name')
-        plt.title('Markov Attributions')
+        plt.title('Markov, order ' + str(self.order))
+        plt.ylabel('Absolute attributed conversions')
+        ax.legend(['Nr conversions', 'Conversion value'])
         plt.show()
 
     def plot_transition_graph(self):
@@ -36,21 +47,29 @@ class MarkovModel:
 
     def plot_removal_effects(self):
         ax = self.removal_effects.plot.bar('channel_name')
-        plt.title('Markov Removal Effects')
+        plt.title('Markov, order ' + str(self.order))
+        plt.ylabel('Normalized attributions')
+        plt.xlabel('Channel')
+        ax.legend(['Nr conversions', 'Conversion value'])
+        plt.tight_layout()
+        plt.savefig('../Plots/Markov_model_order_' + str(self.order) + '_' + str(datetime.now()).replace(' ','_').replace(':','_') + '.png', dpi=200)
         plt.show()
+
 
 
 def load_dataframe(data_file_path):
     dataframe = pd.read_csv(data_file_path, delimiter=';')
+    print(dataframe.dtypes)
     if 'total_null' not in dataframe:
         dataframe['total_null'] = 0
     return dataframe
 
 if __name__ == '__main__':
-    data_file_path = '../Data/GA_data_processed.csv'#'../Data/sample-data-conversion-paths.csv'
-
+    data_file_path = '../Data/channel_journey_data_processed.csv'
+    order = 3
+    show_top_results = True
     dataframe = load_dataframe(data_file_path)
-    markov_model = MarkovModel(dataframe)
-    markov_model.train_all()
-    markov_model.show_results()
+    markovmodel = MarkovModel(dataframe, order, show_top_results)
+    markovmodel.train_all()
+    markovmodel.show_results()
 
