204c24c63878445859d2d7d0a57202f0ae1c831b
viktorrei <viktor_reineck@yahoo.se>
2021-02-09 16:26:15 +0100

    working ga api to fetch data

--- Models/DataProcessing.py
+++ Models/DataProcessing.py
@@ -5,104 +5,118 @@ from sklearn.neighbors import KernelDensity
 from datetime import timedelta
 
 from googleapiclient.discovery import build
-#import googleapiclient
 from oauth2client.service_account import ServiceAccountCredentials
+
 
 class ApiDataGA:
     def __init__(self):
         self.SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
         self.KEY_FILE_LOCATION = '../API/Master-Thesis-GA-api-b6dc4fc6d4dd.json'
-        self.VIEW_ID = '229972923'
+        self.VIEW_ID = open('../API/view_id.txt', 'r').readline()
+        self.analytics = None
+        self.GA_api_df = None
 
     def initialize_analyticsreporting(self):
-        """Initializes an Analytics Reporting API V4 service object.
-        Returns: An authorized Analytics Reporting API V4 service object.
-        """
+        # Initializes an Analytics Reporting API V4 service object.
         credentials = ServiceAccountCredentials.from_json_keyfile_name(self.KEY_FILE_LOCATION, self.SCOPES)
-
-        # Build the service object.
         analytics = build('analyticsreporting', 'v4', credentials=credentials)
-        return analytics
+        self.analytics = analytics
 
-    def get_report(self, analytics):
-        """Queries the Analytics Reporting API V4.
-        Args:
-          analytics: An authorized Analytics Reporting API V4 service object.
-        Returns:
-          The Analytics Reporting API V4 response.
-        """
-        return analytics.reports().batchGet(
+    def create_report_df(
+            self):  ## Queries the Analytics Reporting API V4. Returns the Analytics Reporting API V4 response.
+        METRICS = ['ga:sessions', 'ga:goal1Completions', 'ga:goal1Value']
+        ## dimension6 = cllientId, dimension7 = sessionId, dimension8 = hit timestamp
+        DIMS = ['ga:dimension6', 'ga:dimension7', 'ga:dimension8', 'ga:campaign',
+                'ga:sourcemedium', 'ga:source', 'ga:devicecategory']
+        data = self.analytics.reports().batchGet(
             body={
                 'reportRequests': [
                     {
                         'viewId': self.VIEW_ID,
-                        'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
-                        'metrics': [{'expression': 'ga:sessions'}],
-                        'dimensions': [{'name': 'ga:country'}]
+                        'dateRanges': [{'startDate': '2021-02-01', 'endDate': 'today'}],
+                        'metrics': [{'expression': exp} for exp in METRICS],
+                        'dimensions': [{'name': name} for name in DIMS],
+                        'pageSize': 100000,  ## max nr of query results allowed by api
                     }]
             }
         ).execute()
 
-    def print_response(self,response):
-        """Parses and prints the Analytics Reporting API V4 response.
-        Args:
-          response: An Analytics Reporting API V4 response.
-        """
-        for report in response.get('reports', []):
-            columnHeader = report.get('columnHeader', {})
-            dimensionHeaders = columnHeader.get('dimensions', [])
-            metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
-
-            for row in report.get('data', {}).get('rows', []):
-                dimensions = row.get('dimensions', [])
+        data_dic = {f"{i}": [] for i in DIMS + METRICS}
+        for report in data.get('reports', []):
+            rows = report.get('data', {}).get('rows', [])
+            for row in rows:
+                for i, key in enumerate(DIMS):
+                    data_dic[key].append(row.get('dimensions', [])[i])  # Get dimensions
                 dateRangeValues = row.get('metrics', [])
+                for values in dateRangeValues:
+                    all_values = values.get('values', [])  # Get metric values
+                    for i, key in enumerate(METRICS):
+                        data_dic[key].append(all_values[i])
 
-                for header, dimension in zip(dimensionHeaders, dimensions):
-                    print(header + ': ', dimension)
+        GA_api_df = pd.DataFrame(data=data_dic)
+        GA_api_df.columns = [col.split(':')[-1] for col in GA_api_df.columns]
 
-                for i, values in enumerate(dateRangeValues):
-                    print('Date range:', str(i))
-                    for metricHeader, value in zip(metricHeaders, values.get('values')):
-                        print(metricHeader.get('name') + ':', value)
+        self.GA_api_df = GA_api_df
 
-    def main(self):
-        analytics = self.initialize_analyticsreporting()
-        response = self.get_report(analytics)
-        self.print_response(response)
+    def get_GA_df(self):
+        return self.GA_api_df
+
+    def initialize_api(self):
+        self.initialize_analyticsreporting()
+        self.create_report_df()
+
 
 class DataProcessing:
-    def __init__(self, file_path_GA_main = None, file_path_GA_secondary = None, file_path_mixpanel = None,
-                 file_path_GA_aggregated = None, save_to_path = None):
+    def __init__(self, file_path_mixpanel=None, file_path_GA_aggregated=None, save_to_path=None):
         self.GA_df = None
         self.MP_df = None
         self.converted_clients_df = None
         self.GA_aggregated_df = None
-        self.file_path_GA_main = file_path_GA_main
-        self.file_path_GA_secondary = file_path_GA_secondary
+        #self.file_path_GA_main = file_path_GA_main
+        #self.file_path_GA_secondary = file_path_GA_secondary
         self.file_path_mixpanel = file_path_mixpanel
         self.file_path_GA_aggregated = file_path_GA_aggregated
         self.save_to_path = save_to_path
 
     def process_individual_data(self):
-        df1 = pd.read_csv(self.file_path_GA_main, header=5, dtype={'cllientId': str}) ##Remember to change header=5
-        df2 = pd.read_csv(self.file_path_GA_secondary, header=5, dtype={'cllientId': str})
-        cols_to_use = df2.columns.difference(df1.columns)
-        df = pd.merge(df1, df2[cols_to_use], left_index=True, right_index=True, how='outer')
+        GA_api = ApiDataGA()
+        GA_api.initialize_api()
+        GA_api_df = GA_api.get_GA_df()
+        GA_api_df = GA_api_df.rename(columns={'dimension6': 'client_id',
+                                        'dimension7': 'session_id',
+                                        'campaign': 'campaign',
+                                        'sessions': 'sessions',
+                                        'dimension8': 'timestamp',
+                                        'sourcemedium': 'source_medium',
+                                        'goal1Completions': 'conversion',
+                                        'goal1Value': 'conversion_value',
+                                        'devicecategory': 'device_category'})
 
-        df = df.rename(columns={'cllientId': 'client_id',
-                                'sessionId': 'session_id',
-                                'Campaign': 'campaign',
-                                'Sessions': 'sessions',
-                                'hit timestamp': 'timestamp',
-                                'Source / Medium': 'source_medium',
-                                'Signed Customer (Goal 1 Completions)': 'conversion',
-                                'Signed Customer (Goal 1 Value)': 'conversion_value',
-                                'Device Category': 'device_category'})
         ## Be aware! Check time zones Daylight Savings Time (GA vs. MP)
-        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
-        df['conversion_value'] = df['conversion_value'].str.replace('SEK','').astype(float)
+        GA_api_df['timestamp'] = pd.to_datetime(GA_api_df['timestamp'], utc=True)
+        GA_api_df['conversion'] = GA_api_df['conversion'].astype(int)
+        GA_api_df['conversion_value'] = GA_api_df['conversion_value'].astype(float)
+        GA_api_df['sessions'] = GA_api_df['sessions'].astype(int)
 
-        self.GA_df = df
+        #df1 = pd.read_csv(self.file_path_GA_main, header=5, dtype={'cllientId': str})  ##Remember to change header=5
+        #df2 = pd.read_csv(self.file_path_GA_secondary, header=5, dtype={'cllientId': str})
+        #cols_to_use = df2.columns.difference(df1.columns)
+        #df = pd.merge(df1, df2[cols_to_use], left_index=True, right_index=True, how='outer')
+
+        #df = df.rename(columns={'cllientId': 'client_id',
+        #                        'sessionId': 'session_id',
+        #                        'Campaign': 'campaign',
+        #                        'Sessions': 'sessions',
+        #                        'hit timestamp': 'timestamp',
+        #                        'Source / Medium': 'source_medium',
+        #                        'Signed Customer (Goal 1 Completions)': 'conversion',
+        #                        'Signed Customer (Goal 1 Value)': 'conversion_value',
+        #                        'Device Category': 'device_category'})
+        ## Be aware! Check time zones Daylight Savings Time (GA vs. MP)
+        #df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
+        #df['conversion_value'] = df['conversion_value'].str.replace('SEK', '').astype(float)
+
+        self.GA_df = GA_api_df
 
     def group_by_client_id(self):
         df = self.GA_df.sort_values(by=['client_id', 'timestamp'], ascending=True)
@@ -183,7 +197,8 @@ class DataProcessing:
         for client, conversion_session in conversion_sessions_df.iterrows():
             self.match_client(client, conversion_session, minute_margin, premium_margin)
         nr_conversions = len(self.GA_df.loc[self.GA_df['conversion'] == 1])
-        print('Matched ', len(self.converted_clients_df), ' users of ', nr_conversions)
+        print('Matched ', len(self.converted_clients_df), ' users of ', nr_conversions)# +
+              #'(' + str(len(self.converted_clients_df)/nr_conversions) + '%)')
 
 
     def match_client(self, client, conversion_session, minute_margin, premium_margin):
@@ -246,9 +261,9 @@ class DataProcessing:
         self.estimate_client_LTV()
 
 class Descriptives:
-    def __init__(self, start_time, file_path_GA_main = None, file_path_GA_secondary = None, file_path_mp = None):
+    def __init__(self, start_time, file_path_mp = None):
         self.start_time = start_time
-        self.data_processing = DataProcessing(file_path_GA_main, file_path_GA_secondary, file_path_mp)
+        self.data_processing = DataProcessing(file_path_mp)
         self.GA_df = None
         self.MP_df = None
         self.converted_clients_df = None
@@ -394,11 +409,12 @@ if __name__ == '__main__':
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
 
-    file_path_GA_main = '../Data/Analytics_sample_1.csv'
-    file_path_GA_secondary = '../Data/Analytics_sample_2.csv'
-    file_path_mp = '../Data/Mixpanel_data_2021-02-05.csv'
+    #file_path_GA_main = '../Data/Analytics_sample_1.csv'
+    #file_path_GA_secondary = '../Data/Analytics_sample_2.csv'
+    file_path_mp = '../Data/Mixpanel_data_2021-02-09.csv'
     start_time = pd.Timestamp(year=2021, month=2, day=1, tz='UTC')
 
-    api = ApiDataGA()
-    descriptives = Descriptives(start_time, file_path_GA_main, file_path_GA_secondary, file_path_mp)
+    #data = DataProcessing(file_path_mp)
+    #data.process_all(start_time)
+    descriptives = Descriptives(start_time, file_path_mp)
     descriptives = descriptives.show_interesting_results_combined()
