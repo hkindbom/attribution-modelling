b04adb33b74947caf11f7915635b5b665fc1725e
viktorrei <viktor_reineck@yahoo.se>
2021-02-16 18:43:18 +0100

    fetch channel cost and minor fixes

--- Models/EvaluationFW.py
+++ Models/EvaluationFW.py
@@ -6,13 +6,14 @@ from DataProcessing import DataProcessing
 class Evaluation:
     def __init__(self, start_date, end_date, file_path_mp, nr_top_ch, use_time, train_prop, ratio_maj_min_class,
                  total_budget):
-        self.lr_model = LR(start_date, end_date, file_path_mp, nr_top_ch, use_time, train_prop, ratio_maj_min_class)
-        self.sp_model = SP(start_date, end_date, file_path_mp, nr_top_ch, train_prop, ratio_maj_min_class)
+        #self.lr_model = LR(start_date, end_date, file_path_mp, nr_top_ch, use_time, train_prop, ratio_maj_min_class)
+        #self.sp_model = SP(start_date, end_date, file_path_mp, nr_top_ch, train_prop, ratio_maj_min_class)
         self.data_processing = DataProcessing(start_date, end_date, file_path_mp, nr_top_ch=nr_top_ch,
                                               ratio_maj_min_class = ratio_maj_min_class)
         self.data_processing.process_all()
         self.total_budget = total_budget
-        #self.model =
+        self.model = None
+        self.channels_roi = {}
 
     def train_lr_model(self):
         self.lr_model.train()
@@ -20,9 +21,18 @@ class Evaluation:
     def train_sp_model(self):
         self.sp_model.train()
 
+    def train_model(self):
+        # alternative method
+        self.model.train()
+
     def calculate_roi(self):
         GA_df = self.data_processing.get_GA_df()
+        conversion_paths = GA_df.loc[GA_df['converted_eventually'] == 1]
+
         funnel_df = self.data_processing.get_funnel_df()
+        channels_spend = funnel_df.groupby(['Traffic_source']).agg('sum')['Cost']
+        for channel_name, channel_spend in channels_spend.iteritems():
+            self.channels_roi[channel_name] = None
 
     def calculate_channel_budgets(self):
         pass
@@ -38,6 +48,9 @@ class Evaluation:
 
 
 if __name__ == '__main__':
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+
     file_path_mp = '../Data/Mixpanel_data_2021-02-11.csv'
     start_date = pd.Timestamp(year=2021, month=2, day=3, hour=0, minute=0, tz='UTC')
     end_date = pd.Timestamp(year=2021, month=2, day=10, hour=23, minute=59, tz='UTC')
@@ -46,6 +59,8 @@ if __name__ == '__main__':
     nr_top_ch = 10
     ratio_maj_min_class = 1
     use_time = True
+    total_budget = 100000
 
-    evaluation = Evaluation(start_date, end_date, file_path_mp, nr_top_ch, use_time, train_prop, ratio_maj_min_class)
-    evaluation.back_evaluation()
+    evaluation = Evaluation(start_date, end_date, file_path_mp, nr_top_ch, use_time, train_prop, ratio_maj_min_class,
+                            total_budget)
+    evaluation.calculate_roi()
